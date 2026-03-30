10018679056769604172b9d3514abfb284ac5cf5
hemypo <a1234@iMac-1234.local>
2026-03-30 18:05:06 +0700

    Fix port

--- main.py
+++ main.py
@@ -71,5 +71,5 @@ def tilda_webhook():
 if __name__ == '__main__':
     # Получаем порт из переменной окружения платформы, 
     # либо используем 5000 для локального тестирования
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 80))
     app.run(host='0.0.0.0', port=port)
